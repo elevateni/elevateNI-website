15518ef59a412b1471498032b43013c284f98377
Caitlin <elevateconfni@gmail.com>
2025-01-05 18:21:30 +0000

    adding more text to the tickets section

--- app/page.jsx
+++ app/page.jsx
@@ -114,6 +114,8 @@ export default function Home() {
               <p className="text-gray-300 text-center max-w-2xl">
               Want to network, learn, and grow both personally and professionally?
               </p>
+              <p className="text-gray-300 text-center max-w-2xl">Tickets only £5</p>
+              <p className="text-gray-300 text-center max-w-2xl">100% of Tickets proceeds go directly to charity</p>
               <Link href="https://fixr.co/event/elevateni-student-conference-2025-tickets-627078385">
               <button className="px-6 py-3 bg-gradient-to-r from-[#40E0D0] to-[#9370DB] text-white font-semibold rounded-lg hover:opacity-90 transition-opacity">
                 Get Your Ticket
