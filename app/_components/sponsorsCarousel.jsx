b5386c677ed8e0e14dc1527e0a6796a4c5d0b240
Caitlin <elevateconfni@gmail.com>
2025-01-21 16:53:16 +0000

    updating Liberty IT name in sponsorships

--- app/_components/sponsorsCarousel.jsx
+++ app/_components/sponsorsCarousel.jsx
@@ -14,7 +14,7 @@ export function SponsorsCarousel() {
   const sponsors = [
     {
       id: 1,
-      name: "LibertyIT",
+      name: "Liberty IT",
       logoURL: "/images/Lib.png",
     },
     {
