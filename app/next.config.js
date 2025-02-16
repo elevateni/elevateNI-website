8e410eb9acb490f49ed759a4d96ce7d8f5bf80d6
Caitlin <elevateconfni@gmail.com>
2025-02-16 11:55:38 +0000

    block all external images only load the local ones

--- app/next.config.js
+++ app/next.config.js
@@ -7,8 +7,11 @@ module.exports = {
             { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
             { key: "X-Frame-Options", value: "DENY" },
             { key: "X-Content-Type-Options", value: "nosniff" },
-            { key: "Referrer-Policy", value: "no-referrer-when-downgrade" },
-            { key: "Content-Security-Policy", value: "default-src 'self'; img-src 'self' data:;" },
+            { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+            { 
+                key: "Content-Security-Policy", 
+                value: "default-src 'self'; img-src 'self' data:; script-src 'none'; style-src 'self' 'unsafe-inline';" 
+              },
           ],
         },
       ];
