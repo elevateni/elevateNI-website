f5600135ca5bd07fa77dda896ab53ecc40e95842
Caitlin <elevateconfni@gmail.com>
2025-02-26 11:15:37 +0000

    schedule is dynamic

--- app/_components/schedule.jsx
+++ app/_components/schedule.jsx
@@ -1,6 +1,7 @@
 import '@mobiscroll/react/dist/css/mobiscroll.min.css';
-import { Eventcalendar, setOptions } from '@mobiscroll/react';
-import { useEffect, useMemo, useState } from 'react';
+import { Eventcalendar, setOptions, Toast } from '@mobiscroll/react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import styles from './schedule.css';
 
 setOptions({
   theme: 'ios',
@@ -26,7 +27,7 @@ const events = [
   {
     start: `${initialDate}T11:00:00.000+01:00`,
     end: `${initialDate}T11:30:00.000+01:00`,
-    title: 'Talk2',
+    title: 'Michael Loughran AI Supervision in Drug Dispensing',
     resource: 20,
   },
   {
@@ -42,45 +43,39 @@ const events = [
     resource: 4,
   },
   {
-    start: `${initialDate}T11:00:00.000+01:00`,
-    end: `${initialDate}T11:30:00.000+01:00`,
-    title: 'Main Track 1',
-    resource: 1,
-  },
-  {
     start: `${initialDate}T11:45:00.000+01:00`,
     end: `${initialDate}T12:15:00.000+01:00`,
-    title: 'Main Track 2',
+    title: 'Yuan Zhang From Classroom to Conference Stage How Extracurricular Can Launch Your Tech Career',
     resource: 1,
   },
   {
     start: `${initialDate}T13:00:00.000+01:00`,
     end: `${initialDate}T13:30:00.000+01:00`,
-    title: 'Main Track 3',
+    title:"'Megan D'Arcy & Saffron Baker Beyond Big Brother: The Corporate Takeover",
     resource: 1,
   },
   {
     start: `${initialDate}T13:45:00.000+01:00`,
     end: `${initialDate}T14:15:00.000+01:00`,
-    title: 'Main Track 4',
+    title: 'Matthew Taylor What We Get Wrong About Mental Health: As a former Inpatient at a Psych Ward',
     resource: 1,
   },
   {
     start: `${initialDate}T14:30:00.000+01:00`,
     end: `${initialDate}T15:00:00.000+01:00`,
-    title: 'Main Track 5',
+    title: 'Anna McDermott Playground Curiosity Philosophy As a Tool for Children',
     resource: 1,
   },
   {
     start: `${initialDate}T15:30:00.000+01:00`,
     end: `${initialDate}T16:00:00.000+01:00`,
-    title: 'Main Track 6',
+    title: 'Celine Ostermeyer Women Shaping the Future of Tech',
     resource: 1,
   },
   {
     start: `${initialDate}T16:15:00.000+01:00`,
     end: `${initialDate}T16:45:00.000+01:00`,
-    title: 'Main Track 7',
+    title: 'Alexander Baine Study Smarter Not Harder: The AI Toolkit for Students',
     resource: 1,
   },
   {
@@ -92,19 +87,19 @@ const events = [
   {
     start: `${initialDate}T11:00:00.000+01:00`,
     end: `${initialDate}T12:15:00.000+01:00`,
-    title: 'Extended workshop',
+    title: "Queen's Computing Society Binary Bracelets: Crafting Secret Computer Codes!",
     resource: 3,
   },
   {
     start: `${initialDate}T11:45:00.000+01:00`,
     end: `${initialDate}T12:15:00.000+01:00`,
-    title: 'Workshop 1',
+    title: 'Project Choice Why do Reproductive Rights Still Matter and How can I get Involved in Protecting Them',
     resource: 2,
   },
   {
     start: `${initialDate}T13:00:00.000+01:00`,
     end: `${initialDate}T14:00:00.000+01:00`,
-    title: 'Workshop 2',
+    title: 'Arts Society Water & Wonder: A Journey into Watercolours',
     resource: 2,
   },
   {
@@ -122,38 +117,32 @@ const events = [
   {
     start: `${initialDate}T13:00:00.000+01:00`,
     end: `${initialDate}T13:30:00.000+01:00`,
-    title: 'Workshop 1',
+    title: 'Women in STEM Vision Boards:  Kickstarting your Journey',
     resource: 3,
   },
   {
     start: `${initialDate}T13:45:00.000+01:00`,
     end: `${initialDate}T14:15:00.000+01:00`,
-    title: 'Workshop 2',
+    title: 'Country Western Society Snake in my Boots',
     resource: 3,
   },
   {
     start: `${initialDate}T14:30:00.000+01:00`,
     end: `${initialDate}T15:00:00.000+01:00`,
-    title: 'Workshop 3',
+    title: 'Legs and Kegs Society',
     resource: 3,
   },
   {
     start: `${initialDate}T15:30:00.000+01:00`,
     end: `${initialDate}T16:00:00.000+01:00`,
-    title: 'Workshop 4',
+    title: 'Game Developers Society Works in Progress',
     resource: 3,
   },
   {
     start: `${initialDate}T16:15:00.000+01:00`,
     end: `${initialDate}T16:45:00.000+01:00`,
-    title: 'Workshop 5',
+    title: 'Chess Club Chess for Complete Beginners',
     resource: 3,
-  },
-  {
-    start: `${initialDate}T14:00:00.000+01:00`,
-    end: `${initialDate}T17:00:00.000+01:00`,
-    title: 'Nightline',
-    resource: 1,
   },
   {
     start: `${initialDate}T12:15:00.000+01:00`,
@@ -161,11 +150,25 @@ const events = [
     title: 'Lunch',
     resource: 1,
   },
+  {
+    start: `${initialDate}T17:00:00.000+01:00`,
+    end: `${initialDate}T18:00:00.000+01:00`,
+    title: 'Charity Raffle',
+    resource: 1,
+  },
+  {
+    start: `${initialDate}T19:30:00.000+01:00`,
+    end: `${initialDate}T21:00:00.000+01:00`,
+    title: 'After Party & Quiz',
+    resource: 1,
+  },
 ];
 
 
 
 export function Schedule() {
+  // const [toastText, setToastText] = useState();
+  // const [isToastOpen, setToastOpen] = useState(false);
   const [myEvents, setEvents] = useState([]);
 
   const calView = useMemo(
@@ -175,11 +178,21 @@ export function Schedule() {
         startTime: '09:00',
         endTime: '18:00',
         timeCellStep: 30,
-        currentTimeIndicator: false,
+        currentTimeIndicator: true,
       },
     }),
     [],
   );
+
+  // const handleToastClose = useCallback(() => {
+  //   setToastOpen(false);
+  // }, []);
+
+  // const handleEventClick = useCallback((args) => {
+  //   text = ""
+  //   setToastText(args.event.title);
+  //   setToastOpen(true);
+  // }, []);
   
   const myResources = useMemo(
     () => [
@@ -216,7 +229,7 @@ export function Schedule() {
         <h2 className="text-3xl font-bold mb-12 text-center bg-gradient-to-r from-[#40E0D0] to-[#9370DB] text-transparent bg-clip-text">
           Schedule
         </h2>
-    <div style={{ height: '80vh', overflowY: 'auto' }}> 
+    <div className={styles['schedule-container']}>
     <Eventcalendar
     view={calView}
     data={myEvents}
@@ -226,8 +239,10 @@ export function Schedule() {
       </div>
     )}
     selectedDate={initialDate}
+    // onEventClick={handleEventClick}
   />
   </div>
+  {/* <Toast message={toastText} isOpen={isToastOpen} onClose={handleToastClose} /> */}
   </section>
   );
 }
